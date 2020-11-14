ee5e831ce8a57b7cd20fbf5c279082cd2993fd5e
Max Nicolai <maxvnicolai@gmail.com>
2020-11-13 23:56:32 -0800

    Completely changed request from fetch to axios format. app working

--- zoomCode.js
+++ zoomCode.js
@@ -16,23 +16,28 @@ const zoom = {
       role: 0, // 0 : guest, 1 : host. Locked to guest--
       // host can't currently start meeting from app, but can join again as guest
     };
-
   // Create a string with the data required to generate a signature
   // Passing the obj directly causes a CORS error, I believe this is more secure
   const raw = JSON.stringify({
-    meetingNumber: zoomConfig.id,
+    meetingNumber: zoomConfig.meetingNumber,
     role: zoomConfig.role,
   });
 
-  const meetingConfig = await axios("https://ttzoomsignature.herokuapp.com/", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: raw,
+  console.log(typeof raw)
+
+  const meetingConfig = axios({
+    method: "post",
+    url: "https://ttzoomsignature.herokuapp.com/",
+    data: {
+      meetingNumber: zoomConfig.meetingNumber,
+      role: zoomConfig.role,
+    }
   })
     // Execute on success
     .then((response) => {
       // Add signature k/v pair to config object defined above
       // This allows the entire config to be returned as an object, simplifying the browser side code
+      console.log(response.data.signature)
       zoomConfig.signature = response.data.signature
       //Pass the completed object back to parent
       return zoomConfig;
